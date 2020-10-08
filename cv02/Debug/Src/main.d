ab788be3c9ce73b947bd02ec821774640d2718e7
xmaluc00@vutbr.cz <xmaluc00@vutbr.cz>
2020-10-09 00:18:56 +0200

    bod 2.4

--- cv02/Debug/Src/main.d
+++ cv02/Debug/Src/main.d
@@ -1,19 +1,19 @@
 Src/main.o: ../Src/main.c \
- C:/Users/pavelm/Documents/xmaluc00/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
- C:/Users/pavelm/Documents/xmaluc00/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
- C:/Users/pavelm/Documents/xmaluc00/MKS/cv01/CMSIS/Include/core_cm0.h \
- C:/Users/pavelm/Documents/xmaluc00/MKS/cv01/CMSIS/Include/core_cmInstr.h \
- C:/Users/pavelm/Documents/xmaluc00/MKS/cv01/CMSIS/Include/core_cmFunc.h \
- C:/Users/pavelm/Documents/xmaluc00/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
+ C:/Users/pavelm/Documents/xmaluc00/MKS/cv02/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ C:/Users/pavelm/Documents/xmaluc00/MKS/cv02/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ C:/Users/pavelm/Documents/xmaluc00/MKS/cv02/CMSIS/Include/core_cm0.h \
+ C:/Users/pavelm/Documents/xmaluc00/MKS/cv02/CMSIS/Include/core_cmInstr.h \
+ C:/Users/pavelm/Documents/xmaluc00/MKS/cv02/CMSIS/Include/core_cmFunc.h \
+ C:/Users/pavelm/Documents/xmaluc00/MKS/cv02/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
 
-C:/Users/pavelm/Documents/xmaluc00/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+C:/Users/pavelm/Documents/xmaluc00/MKS/cv02/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
 
-C:/Users/pavelm/Documents/xmaluc00/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
+C:/Users/pavelm/Documents/xmaluc00/MKS/cv02/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
 
-C:/Users/pavelm/Documents/xmaluc00/MKS/cv01/CMSIS/Include/core_cm0.h:
+C:/Users/pavelm/Documents/xmaluc00/MKS/cv02/CMSIS/Include/core_cm0.h:
 
-C:/Users/pavelm/Documents/xmaluc00/MKS/cv01/CMSIS/Include/core_cmInstr.h:
+C:/Users/pavelm/Documents/xmaluc00/MKS/cv02/CMSIS/Include/core_cmInstr.h:
 
-C:/Users/pavelm/Documents/xmaluc00/MKS/cv01/CMSIS/Include/core_cmFunc.h:
+C:/Users/pavelm/Documents/xmaluc00/MKS/cv02/CMSIS/Include/core_cmFunc.h:
 
-C:/Users/pavelm/Documents/xmaluc00/MKS/cv01/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+C:/Users/pavelm/Documents/xmaluc00/MKS/cv02/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
--- cv02/Src/main.c
+++ cv02/Src/main.c
@@ -3,6 +3,9 @@
 static volatile uint32_t Tick;
 
 #define LED_TIME_BLINK 300
+#define BUTTON_DEBOUNCE 40
+#define LED_TIME_SHORT 100
+#define LED_TIME_LONG 1000
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
@@ -11,12 +14,10 @@ static volatile uint32_t Tick;
 
 void EXTI0_1_IRQHandler(void)
 {
-
 	if (EXTI->PR & EXTI_PR_PR0) { // check line 0 has triggered the IT
 	EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
 	GPIOB->ODR ^= (1<<0); // toggle ;
 	}
-
 }
 
 
@@ -33,6 +34,45 @@ void blikac(void)
 		GPIOA->ODR ^= (1<<4);
 		delay = Tick;
 	}
+}
+
+
+
+void tlacitka(void)
+{
+	static uint32_t off_time;
+	static uint32_t debounce1;
+
+	if (Tick > debounce1 + BUTTON_DEBOUNCE){
+
+		static uint32_t old_s2;
+		uint32_t new_s2 = GPIOC->IDR & (1<<0);
+		static uint32_t old_s1;
+		uint32_t new_s1 = GPIOC->IDR & (1<<1);
+
+		if (old_s2 && !new_s2) { // falling edge S2
+
+			off_time = Tick + LED_TIME_SHORT;
+			GPIOB->BSRR = (1<<0);
+
+		 }
+
+		else if (old_s1 && !new_s1) { // falling edge S1
+
+			off_time = Tick + LED_TIME_LONG;
+			GPIOB->BSRR = (1<<0);
+
+		 }
+
+		 old_s2 = new_s2;
+		 old_s1 = new_s1;
+
+	}
+
+	if (Tick > off_time) {
+		GPIOB->BRR = (1<<0);
+	}
+
 }
 
 
