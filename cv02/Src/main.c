5093ff5cd01b754554412e25cbb01b2b84f6ab63
xmaluc00@vutbr.cz <xmaluc00@vutbr.cz>
2020-10-08 23:36:02 +0200

    cv02 bod 2.3

--- cv02/Src/main.c
+++ cv02/Src/main.c
@@ -1,5 +1,9 @@
 #include "stm32f0xx.h"
 
+static volatile uint32_t Tick;
+
+#define LED_TIME_BLINK 300
+
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
@@ -16,6 +20,22 @@ void EXTI0_1_IRQHandler(void)
 }
 
 
+void SysTick_Handler(void)
+{
+	Tick++;
+}
+
+
+void blikac(void)
+{
+	static uint32_t delay;
+	if (Tick > delay + LED_TIME_BLINK) {
+		GPIOA->ODR ^= (1<<4);
+		delay = Tick;
+	}
+}
+
+
 int main(void)
 {
 
@@ -27,13 +47,18 @@ int main(void)
 
 	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
 
-	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
-	EXTI->IMR |= EXTI_IMR_MR0; // mask
-	EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
-	NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
+	SysTick_Config(8000); // 1ms
+
+//	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
+//	EXTI->IMR |= EXTI_IMR_MR0; // mask
+//	EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
+//	NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
 
 
 	for (;;){
+
+		blikac();
+
 	}
 
 }
