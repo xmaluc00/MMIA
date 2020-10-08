6e10f8be51d757be687100857b5a9ac6f656f4b8
xmaluc00@vutbr.cz <xmaluc00@vutbr.cz>
2020-10-08 22:39:27 +0200

    cv02 bod 2.2

--- cv02/Src/main.c
+++ cv02/Src/main.c
@@ -5,6 +5,17 @@
 #endif
 
 
+void EXTI0_1_IRQHandler(void)
+{
+
+	if (EXTI->PR & EXTI_PR_PR0) { // check line 0 has triggered the IT
+	EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
+	GPIOB->ODR ^= (1<<0); // toggle ;
+	}
+
+}
+
+
 int main(void)
 {
 
@@ -13,6 +24,13 @@ int main(void)
 	GPIOB->MODER |= GPIO_MODER_MODER0_0; // LED2 = PB0, output
 	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR0_0; // S2 = PC0, pullup
 	GPIOC->PUPDR |= GPIO_MODER_MODER1_0; // S1 = PC1, pullup
+
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+
+	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
+	EXTI->IMR |= EXTI_IMR_MR0; // mask
+	EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
+	NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
 
 
 	for (;;){
