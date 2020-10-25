ba72d76d72225245b63a156df57d98842a344c7f
xmaluc00 <xmaluc00@vutbr.cz>
2020-10-26 00:07:49 +0100

    cv04

---
+++ cv04/Core/Inc/sct.h
@@ -0,0 +1,8 @@
+#ifndef SCT_H_
+#define SCT_H_
+
+void sct_led (uint32_t value);
+void sct_init (void);
+void sct_value (uint16_t value, uint8_t led);
+
+#endif /* SCT_H_ */
--- cv3/Inc/sct.h
+++
@@ -1,8 +0,0 @@
-#ifndef SCT_H_
-#define SCT_H_
-
-void sct_led (uint32_t value);
-void sct_init (void);
-void sct_value (uint16_t value);
-
-#endif /* SCT_H_ */
